3cda680c9751b369c3e98be71f14c896c2a31e3e
Adel <576853-astran@users.noreply.gitlab.com>
2022-12-20 23:40:01 +0100

    test svelte calc

---
+++ sveltekit/todo-app/src/routes/add/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	let a = 0;
+	let b = 0;
+	let total = 0;
+
+	async function add() {
+		const response = await fetch('/api/add', {
+			method: 'POST',
+			body: JSON.stringify({ a, b }),
+			headers: {
+				'content-type': 'application/json'
+			}
+		});
+
+		total = await response.json();
+	}
+	async function randomValues() {
+		a =await fetchRandomValue();
+		b =await fetchRandomValue();
+	}
+	async function fetchRandomValue(): Promise<number> {
+		const response = await fetch('/api/random-number?min=1&max=100', {
+			method: 'GET'
+		});
+
+		return await response.json();
+	}
+</script>
+
+<input type="number" bind:value={a} /> +
+<input type="number" bind:value={b} /> =
+{total}
+
+<button on:click={add}>Calculate</button>
+
+<button on:click={randomValues}>random data</button>
