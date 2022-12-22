06a8b883ea2737a244ddda8e2bae00821bbfe593
Adel <576853-astran@users.noreply.gitlab.com>
2022-12-22 09:14:49 +0100

    fix typing

--- nextjs/next-todo/pages/index.tsx
+++ nextjs/next-todo/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Alert from '../components/alert';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 
@@ -9,6 +10,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
+        <Alert type='error'><h1>mazleaze</h1></Alert>
         <p>[Your Self Introduction]</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
