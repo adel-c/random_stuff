1e8dbb97621e28a80edf5364bba01c30ef16d2f2
Adel <576853-astran@users.noreply.gitlab.com>
2023-03-26 19:58:36 +0200

    aze

--- rust/guessing_game/target/debug/deps/guessing_game-61022fda68e33920.d
+++
@@ -1,5 +0,0 @@
-/home/adel/projects/random_stuff/rust/guessing_game/target/debug/deps/guessing_game-61022fda68e33920: src/main.rs
-
-/home/adel/projects/random_stuff/rust/guessing_game/target/debug/deps/guessing_game-61022fda68e33920.d: src/main.rs
-
-src/main.rs:
--- rust/guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/home/adel/projects/random_stuff/rust/guessing_game/target/debug/guessing_game: /home/adel/projects/random_stuff/rust/guessing_game/src/main.rs
