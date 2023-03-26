5ec74a58d95ac2c77103593f9a12b42bb5275cc6
Adel <576853-astran@users.noreply.gitlab.com>
2023-03-26 21:30:02 +0200

    helm

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -1,3 +1,8 @@
+use std::io;
+use rand::Rng;
+const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
 fn main() {
-    println!("Hello, world!");
-}
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");}
