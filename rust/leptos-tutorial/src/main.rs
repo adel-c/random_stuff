95b0dcbc5a77761bf038108ab9b212fca345bc3c
Adel <576853-astran@users.noreply.gitlab.com>
2023-05-07 12:39:13 +0200

    leptos first component

--- rust/leptos-tutorial/src/main.rs
+++ rust/leptos-tutorial/src/main.rs
@@ -1,3 +1,47 @@
+use leptos::*;
+
 fn main() {
-    println!("Hello, world!");
+    mount_to_body(|cx| view! { cx, <App/> })
+}
+
+#[component]
+fn App(cx: Scope) -> impl IntoView {
+    let (count, set_count) = create_signal(cx, 0);
+    let double_count = move || count() * 2;
+    let triple_count = move || count() * 3;
+    view! { cx,
+        <button
+            class:red=move || count() % 2 == 1
+            on:click=move |_| {
+                set_count.update(|n| *n += 1);
+            }
+        >
+            "Click me: "
+            {count}
+        </button>
+        <ProgressBar progress=count max=100 />
+        <ProgressBar progress=double_count max=100/>
+        <ProgressBar progress=triple_count max=100 />
+    }
+}
+
+
+#[component]
+fn ProgressBar<F>(
+    cx: Scope,
+    #[prop(default = 100)]
+    max: u16,
+    progress:  F,
+) -> impl IntoView
+where F : Fn() -> i32 +'static{
+    view! { cx,
+        <div>
+        <progress
+            max=max
+            // hmm... where will we get this from?
+            value=progress
+        />
+        </div>
+
+    }
 }
