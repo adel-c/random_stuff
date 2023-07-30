b2fcd187b6730d2f31551a51548da47bdf67381f
Adel <576853-astran@users.noreply.gitlab.com>
2023-07-30 23:17:22 +0200

    wip

--- java/htmx_router/src/main/java/com/ace/htmxrouter/MainController.java
+++ java/htmx_router/src/main/java/com/ace/htmxrouter/MainController.java
@@ -1,10 +1,26 @@
 package com.ace.htmxrouter;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
+@AllArgsConstructor
 public class MainController {
-
+    PageService pageService;
+    @GetMapping()
+    public String main(Model model){
+        pageService.loadMainDate(model);
+        model.addAttribute("fragment","/page/main");
+        return "index";
+    }
+    @GetMapping("/about")
+    public String about(Model model){
+        pageService.loadMainDate(model);
+        model.addAttribute("fragment","/page/about");
+        return "index";
+    }
 }
--- java/htmx_router/src/main/java/com/ace/htmxrouter/PageService.java
+++ java/htmx_router/src/main/java/com/ace/htmxrouter/PageService.java
@@ -1,6 +1,17 @@
 package com.ace.htmxrouter;
 
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import java.util.Date;
+
+@Service
 public class PageService {
 
     public void stuff(){}
+
+    public void loadMainDate(Model model) {
+        model.addAttribute("time", new Date());
+        model.addAttribute("name","coucou");
+    }
 }
