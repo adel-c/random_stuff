521b76555742cdfce9cb47ad63d7fdc213c0a4fc
Adel <576853-astran@users.noreply.gitlab.com>
2023-10-03 22:38:26 +0200

    wip

--- go/test_gui/main.go
+++ go/test_gui/main.go
@@ -1,18 +1,36 @@
-
-
 package main
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/widget"
+	"time"
 )
+
+func updateTime(clock *widget.Label) {
+	formatted := time.Now().Format("Time: 03:04:05")
+	clock.SetText(formatted)
+}
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Hello World")
+	w := a.NewWindow("Clock")
 
-	w.SetContent(widget.NewLabel("Hello World!"))
-	w.ShowAndRun()
+	clock := widget.NewLabel("")
+	updateTime(clock)
+
+	w.SetContent(clock)
+	go func() {
+		for range time.Tick(time.Second) {
+			updateTime(clock)
+		}
+	}()
+
+	w2 := a.NewWindow("Larger")
+	w2.SetContent(widget.NewLabel("More content"))
+	w2.Resize(fyne.NewSize(100, 100))
+	w2.Show()
+
+	w.Show()
+	a.Run()
 }
-
-
