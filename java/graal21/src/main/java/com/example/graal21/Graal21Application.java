72cafda81dc17da6346d9133baa4736be1ea9a74
Adel <576853-astran@users.noreply.gitlab.com>
2024-01-29 22:51:14 +0100

    ddd

--- java/graal21/src/main/java/com/example/graal21/Graal21Application.java
+++ java/graal21/src/main/java/com/example/graal21/Graal21Application.java
@@ -2,12 +2,36 @@ package com.example.graal21;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.web.servlet.function.RequestPredicates.GET;
+import static org.springframework.web.servlet.function.RouterFunctions.route;
 
 @SpringBootApplication
+
 public class Graal21Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Graal21Application.class, args);
 	}
 
+	@Bean
+	RouterFunction<ServerResponse> routes(CustomerRepository customerRepository) {
+		return route(GET("/customer"),
+				request -> ServerResponse.ok().body(customerRepository.findAll()));
+	}
+
+
+}
+@Repository
+interface CustomerRepository extends ListCrudRepository<Customer, Integer> {
+
+}
+
+record Customer(@Id Integer id, String name) {
 }
