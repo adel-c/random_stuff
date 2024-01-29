812575fa9645076a586a0349ad637612eaf9cabd
Adel <576853-astran@users.noreply.gitlab.com>
2024-01-29 23:28:37 +0100

    ddd

--- java/graal21/src/main/java/com/example/graal21/Graal21Application.java
+++ java/graal21/src/main/java/com/example/graal21/Graal21Application.java
@@ -5,9 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.repository.ListCrudRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
+
+import java.util.Map;
 
 import static org.springframework.web.servlet.function.RequestPredicates.GET;
 import static org.springframework.web.servlet.function.RouterFunctions.route;
@@ -23,10 +28,28 @@ public class Graal21Application {
 	@Bean
 	RouterFunction<ServerResponse> routes(CustomerRepository customerRepository) {
 		return route(GET("/customer"),
-				request -> ServerResponse.ok().body(customerRepository.findAll()));
+				request -> ServerResponse.ok().body(customerRepository.findAll()))
+				.and(route(GET("/machin"), request ->
+						ServerResponse.ok().render("customers", Map.of("customers", customerRepository.findAll()))));
 	}
 
 
+}
+
+@Controller
+class CustomerController {
+	CustomerRepository customerRepository;
+
+	CustomerController(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
+
+	@GetMapping("/customers")
+	public String customers(ModelMap map) {
+		map.put("customers", customerRepository.findAll());
+		return "customers";
+
+	}
 }
 interface CustomerRepository extends ListCrudRepository<Customer, Integer> {
 
