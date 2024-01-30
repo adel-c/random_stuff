9ff4aac3d7b6d8b56c52a5bfd5ebc3ef1c8d139e
Adel <576853-astran@users.noreply.gitlab.com>
2024-01-30 07:32:53 +0100

    spring shell kotlin

--- java/shell/src/main/kotlin/com/example/shell/ShellApplication.kt
+++ java/shell/src/main/kotlin/com/example/shell/ShellApplication.kt
@@ -2,10 +2,24 @@ package com.example.shell
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.shell.standard.ShellComponent
+import org.springframework.shell.standard.ShellMethod
+import org.springframework.shell.standard.ShellOption
 
 @SpringBootApplication
 class ShellApplication
 
 fun main(args: Array<String>) {
 	runApplication<ShellApplication>(*args)
+}
+
+
+@ShellComponent
+class MyCommands {
+	@ShellMethod(key = ["hello-world"])
+	fun helloWorld(
+		@ShellOption(defaultValue = "spring") arg: String
+	): String {
+		return "Hello world $arg"
+	}
 }
