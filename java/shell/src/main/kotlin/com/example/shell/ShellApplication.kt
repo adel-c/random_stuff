19bacff16ce88d21b1632092df7c8f8ee7d4027e
Adel <576853-astran@users.noreply.gitlab.com>
2024-02-22 07:49:04 +0100

    test

--- java/shell/src/main/kotlin/com/example/shell/ShellApplication.kt
+++ java/shell/src/main/kotlin/com/example/shell/ShellApplication.kt
@@ -1,10 +1,17 @@
 package com.example.shell
 
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import kotlinx.coroutines.runBlocking
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.shell.standard.ShellComponent
 import org.springframework.shell.standard.ShellMethod
 import org.springframework.shell.standard.ShellOption
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.service.annotation.GetExchange
 
 @SpringBootApplication
 class ShellApplication
@@ -17,9 +24,26 @@ fun main(args: Array<String>) {
 @ShellComponent
 class MyCommands {
 	@ShellMethod(key = ["hello-world"])
-	fun helloWorld(
+	 fun helloWorld(
 		@ShellOption(defaultValue = "spring") arg: String
 	): String {
-		return "Hello world $arg"
+		val r= runBlocking {
+			val client = HttpClient(CIO)
+			val response: HttpResponse = client.get("https://ktor.io/")
+			println(response.status)
+			println()
+			client.close()
+			response.bodyAsText()
+		}
+
+		return "Hello world ${r}"
 	}
+}
+
+internal interface RepositoryService {
+	@GetExchange("/")
+	fun getRepository(
+		@PathVariable owner: String?,
+		@PathVariable repo: String?
+	): String // more HTTP exchange methods...
 }
