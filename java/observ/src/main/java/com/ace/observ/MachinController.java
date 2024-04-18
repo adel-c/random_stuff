958a394e95185bd9ec2617f32b83a9911d374b2f
Adel <576853-astran@users.noreply.gitlab.com>
2024-04-18 11:40:19 +0200

    sample with prometheus ok

--- java/observ/src/main/java/com/ace/observ/MachinController.java
+++ java/observ/src/main/java/com/ace/observ/MachinController.java
@@ -1,13 +1,21 @@
 package com.ace.observ;
 
+import io.micrometer.observation.annotation.Observed;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class MachinController {
 
+
+    @Observed(name = "user.name",
+            contextualName = "getting-user-name",
+            lowCardinalityKeyValues = {"userType", "userType2"})
     @GetMapping("/hello")
     public String machin() {
+      log.info("machin");
         return "coucou";
     }
 }
