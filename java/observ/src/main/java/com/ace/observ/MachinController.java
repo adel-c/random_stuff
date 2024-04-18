071d5b22ffe45c9e566a7f2fbd8d337eaa17d0ee
Adel <576853-astran@users.noreply.gitlab.com>
2024-04-18 10:42:57 +0200

    add prometheus and graphana

--- java/observ/src/main/java/com/ace/observ/MachinController.java
+++ java/observ/src/main/java/com/ace/observ/MachinController.java
@@ -1,4 +1,13 @@
 package com.ace.observ;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MachinController {
+
+    @GetMapping("/hello")
+    public String machin() {
+        return "coucou";
+    }
 }
