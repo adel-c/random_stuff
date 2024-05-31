72e1f0304c1a149cb87d95e00769939a032bf47e
Adel <576853-astran@users.noreply.gitlab.com>
2024-06-01 00:43:38 +0200

    add sample data

--- java/thymleafDemo/src/main/java/com/ace/thymleafdemo/ThymleafDemoApplication.java
+++ java/thymleafDemo/src/main/java/com/ace/thymleafdemo/ThymleafDemoApplication.java
@@ -1,17 +1,17 @@
 package com.ace.thymleafdemo;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.repository.ListCrudRepository;
-import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -26,13 +26,11 @@ public class ThymleafDemoApplication {
 
 }
 
+@AllArgsConstructor
 @RestController
 class UserController {
     private final UserRepository userRepository;
 
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
 
     @GetMapping("/users")
     public List<User> users() {
@@ -47,11 +45,14 @@ interface UserRepository extends ListCrudRepository<User, Integer> {
 @Entity
 @Table(name = "USERS")
 @NoArgsConstructor
+@Getter
 class User {
     @Id
     private Integer id;
     private String name;
+    @JsonIgnore
     private String salt;
+    @JsonIgnore
     private String password;
     private String email;
     private String role;
