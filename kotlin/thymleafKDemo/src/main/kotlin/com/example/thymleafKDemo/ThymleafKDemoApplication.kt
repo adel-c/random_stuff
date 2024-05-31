7eb4dc024cafea1354ae00738c9dacb5fddcef54
Adel <576853-astran@users.noreply.gitlab.com>
2024-05-31 23:08:28 +0200

    kthym

---
+++ kotlin/thymleafKDemo/src/main/kotlin/com/example/thymleafKDemo/ThymleafKDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.thymleafKDemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ThymleafKDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<ThymleafKDemoApplication>(*args)
+}
