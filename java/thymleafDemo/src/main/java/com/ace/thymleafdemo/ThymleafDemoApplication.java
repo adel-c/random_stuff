dec10b8b8940f1bcc5dd6565eeadd0910ab4257a
Adel <576853-astran@users.noreply.gitlab.com>
2024-06-01 00:09:20 +0200

    add compose

--- java/thymleafDemo/src/main/java/com/ace/thymleafdemo/ThymleafDemoApplication.java
+++ java/thymleafDemo/src/main/java/com/ace/thymleafdemo/ThymleafDemoApplication.java
@@ -1,7 +1,11 @@
 package com.ace.thymleafdemo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.Serializable;
 
 @SpringBootApplication
 public class ThymleafDemoApplication {
@@ -10,4 +14,9 @@ public class ThymleafDemoApplication {
         SpringApplication.run(ThymleafDemoApplication.class, args);
     }
 
+
+
+    @Entity
+    @Table(name = "USERS")
+    record User(Integer id, String name,String salt,String password,String email,String role) implements Serializable {}
 }
